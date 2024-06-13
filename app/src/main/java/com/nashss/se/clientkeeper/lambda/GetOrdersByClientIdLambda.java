72f9a878838d39a9f070a5ed3e5413bc8dd20d5d
Chassidy McCray <mccraychassidy@gmail.com>
2024-06-12 20:47:58 -0500

    created front end for completed orders page

---
+++ app/src/main/java/com/nashss/se/clientkeeper/lambda/GetOrdersByClientIdLambda.java
@@ -0,0 +1,25 @@
+package com.nashss.se.clientkeeper.lambda;
+
+import com.nashss.se.clientkeeper.activity.requests.GetOrdersByClientIdRequest;
+import com.nashss.se.clientkeeper.activity.results.GetOrdersByClientIdResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * AWS Lambda function handler for getting orders by clientId.
+ */
+public class GetOrdersByClientIdLambda extends LambdaActivityRunner<GetOrdersByClientIdRequest, GetOrdersByClientIdResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<GetOrdersByClientIdRequest>, LambdaResponse> {
+
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<GetOrdersByClientIdRequest> input, Context context) {
+        return super.runActivity(
+                () -> input.fromPath(path ->
+                        GetOrdersByClientIdRequest.builder()
+                                .withClientId(path.get("clientId"))
+                                .build()),
+                (request, serviceComponent) ->
+                        serviceComponent.provideGetOrdersByClientIdActivity().handleRequest(request)
+        );
+    }
+}
